99ad1ddcf5765d46c21266cb64bcede4f5cb84b4
tangchen2018 <tangchen2018@hotmail.com>
2018-10-24 00:28:59 +0800

    upd

--- install/install.sh
+++ install/install.sh
@@ -2,7 +2,7 @@
 
 cd /root
 yum install -y openssl wget gcc-c++ pcre pcre-devel zlib zlib-devel openssl openssl-devel libssl-dev
-mkdir -p /var/upload/1
+mkdir -p /var/upload
 tar -zxvf nginx-1.6.2.tar.gz 
 tar -zxvf nginx-upload-module.tar.gz
 cd nginx-1.6.2
