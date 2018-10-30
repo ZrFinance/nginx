bd11938ed99fb81b35314b257e8bb9feebe7ee6f
tangchen2018 <tangchen2018@hotmail.com>
2018-10-30 23:28:31 +0800

    upd

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,6 @@ FROM centos
 MAINTAINER tangchen tangchen2018@hotmail.com
 
 COPY install /root
-COPY web /root
-COPY admin /root
 RUN sh /root/install.sh
 
 RUN rm -rf /usr/local/nginx/conf/nginx.conf /usr/local/nginx/conf/conf.d
--- install/install.sh
+++ install/install.sh
@@ -9,16 +9,3 @@ cd nginx-1.6.2
 make
 make install
 
-yum -y install npm
-cd /root/web
-npm install 
-npm run build
-
-cd /root/admin
-npm install 
-npm run build
-
-rm -rf /usr/local/nginx/html/*
-mkdir -p /usr/local/nginx/html/serveradmin
-mv /root/web/dist/* /usr/local/nginx/html/
-mv /root/web/dist/* /usr/local/nginx/html/serveradmin
